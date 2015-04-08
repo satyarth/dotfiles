16f7f02f4b01d35a9842eb95f74abf453b51441e
satyarth.mishra@gmail.com <satyarth.mishra@gmail.com>
2015-04-08 20:58:49 +0100

    Update gitignore

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOTFILES_DIR=/home/satyarth/dotfiles/
+FIREFOX_PROFILE=fc0mrqe8
 
 ln -s $DOTFILES_DIR/herbstluftwm/ ~/.config/herbstluftwm
 ln -s $DOTFILES_DIR/sxhkd/ ~/.config/sxhkd
@@ -8,6 +9,9 @@ ln -s $DOTFILES_DIR/zathura/ ~/.config/zathura
 ln -s $DOTFILES_DIR/.Xresources ~/.Xresources
 ln -s $DOTFILES_DIR/.bashrc ~/.bashrc
 ln -s $DOTFILES_DIR/ncmpcpp/ ~/.ncmpcpp
+ln -s $DOTFILES_DIR/mpd/ ~/.mpd
+ln -s $DOTFILES_DIR/weechat/ ~/.weechat
+ln -s $DOTFILES_DIR/userChrome.css ~/.mozilla/firefox/$FIREFOX_PROFILE.default/chrome/userChrome.css
 
 # Kill recursive symlinks
 find $DOTFILES_DIR -lname '*' -exec rm {} \;
