e90d6eceb743a15f08d2ea52e12c0942bfee8dd7
satyarth.mishra@gmail.com <satyarth.mishra@gmail.com>
2015-04-08 19:43:30 +0100

    Initial commit

---
+++ symlink.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOTFILES_DIR=/home/satyarth/dotfiles/
+
+ln -s $DOTFILES_DIR/herbstluftwm/ ~/.config/herbstluftwm
+ln -s $DOTFILES_DIR/sxhkd/ ~/.config/sxhkd
+ln -s $DOTFILES_DIR/zathura/ ~/.config/zathura
