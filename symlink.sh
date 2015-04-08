e1dc6c09e1bcb0bc2bb546de7adab5dfa9e57b5f
satyarth.mishra@gmail.com <satyarth.mishra@gmail.com>
2015-04-08 19:51:39 +0100

    add Xresources, bashrc

--- symlink.sh
+++ symlink.sh
@@ -5,3 +5,8 @@ DOTFILES_DIR=/home/satyarth/dotfiles/
 ln -s $DOTFILES_DIR/herbstluftwm/ ~/.config/herbstluftwm
 ln -s $DOTFILES_DIR/sxhkd/ ~/.config/sxhkd
 ln -s $DOTFILES_DIR/zathura/ ~/.config/zathura
+ln -s $DOTFILES_DIR/.Xresources ~/.Xresources
+ln -s $DOTFILES_DIR/.bashrc ~/.bashrc
+
+# Kill recursive symlinks
+find $DOTFILES_DIR -lname '*' -exec rm {} \;
