b79508a8fb9a4523e9c13967e7f77f663591d655
Satyarth Mishra Sharma <mail@satyarth.me>
2015-06-24 13:55:09 +0100

    Temporarily fix dzen popups

--- scripts/dzen-popup.sh
+++ scripts/dzen-popup.sh
@@ -4,16 +4,11 @@ dzen2bg="#3f3f3f"
 dzen2fg="#ffffff"
 font='Tewi'
 
-source ~/.colors/colors.sh
-
 read input_string
 strlen=${#input_string}
 width=`echo $strlen*6 + 12 | bc`
 
-color_number=$RANDOM
-let "color_number %= 8"
-color=color${color_number}
-output_string="^fg(${!color})$input_string^fg() "
+output_string="$input_string"
 
 
 (echo $output_string; sleep 2)| dzen2 -fn $font -fg $dzen2fg -bg $dzen2bg -x 32 -y 32 -w $width -ta c & sleep 2
