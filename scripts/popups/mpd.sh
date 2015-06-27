12302a5597dfe447bfaad5eff72a27e435c3f6f3
Satyarth Mishra Sharma <mail@satyarth.me>
2015-06-27 10:40:25 +0100

    Update mpd popups

--- scripts/popups/mpd.sh
+++ scripts/popups/mpd.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-source ~/.colors/colors.sh
-
 dzen2bg="#3f3f3f"
 dzen2fg="#ffffff"
 font='Tewi'
 
-nowplaying=$(ncmpcpp --now-playing)
+nowplaying=$(mpc | head -n1)
 strlen=${#nowplaying}
 width=`echo $strlen*6 + 12 | bc`
-output="^fg($color8)$nowplaying^fg()"
+output="$nowplaying"
 
 (echo $output; sleep 2)| dzen2 -fn $font -fg $dzen2fg -bg $dzen2bg -x 32 -y 32 -w $width -ta l & sleep 2
