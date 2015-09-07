117a6fcd14f99f90157490a0d08a125a18e83af7
Satyarth Mishra Sharma <mail@satyarth.me>
2015-09-07 23:37:05 +0300

    Initial color-format.py

---
+++ colors/color-format.py
@@ -0,0 +1,40 @@
+import sys
+import argparse
+
+p = argparse.ArgumentParser()
+p.add_argument("filename")
+args = p.parse_args()
+
+def read(args):
+    colors = {}
+    with open(args.filename) as file:
+        for row in file:
+            if 'color' in row:
+                row = row.replace('*color','')
+                index = int(row[0:row.find(':')].strip())
+                color = row[row.find('#'):].strip()
+                colors[index] = color
+    return colors
+
+def output_x(colors):
+    with open('colors.x', 'w') as outfile:
+        for index in colors:
+            outfile.write('*color' + str(index) + ': ' + colors[index] + '\n')
+
+def output_sh(colors):
+    with open('colors.sh', 'w') as outfile:
+        for index in colors:
+            outfile.write('color' + str(index) + '=' + colors[index] + '\n')
+
+def output_css(colors):
+    with open('colors.css', 'w') as outfile:
+        outfile.write(":root {\n")
+        for index in colors:
+            outfile.write('--color' + str(index) + ':' + colors[index] + ';\n')
+        outfile.write("}\n")
+
+colors = read(args)
+
+output_x(colors)
+output_sh(colors)
+output_css(colors)
