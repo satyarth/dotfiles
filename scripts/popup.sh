ef68a222c2b22fc8bfef30825f74edb58bbeed70
Satyarth Mishra Sharma <mail@satyarth.me>
2015-10-08 02:08:20 +0300

    Colorize popups

--- scripts/popup.sh
+++ scripts/popup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-dzen2bg="#3f3f3f"
-dzen2fg="#ffffff"
+source ~/dotfiles/colors/colors.sh
+
 font='-*-fixed-medium-*-*-*-12-*-*-*-*-*-*-*'
 
 read input_string
 strlen=${#input_string}
-width=`echo $strlen*6 | bc`
+width=`echo $strlen*6+18 | bc`
 
-output_string="$input_string"
+output_string="%{c}$input_string"
 
-(echo $output_string; sleep 2)| lemonbar -g ${width}x20+32+32 -f $font & sleep 2
+(echo $output_string; sleep 2)| (lemonbar -g ${width}x20+32+32 -f $font -B$color2 -F#ff000000)
