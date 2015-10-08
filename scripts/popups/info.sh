68fc50cfab0726a851f59f1e61e52fd93bc10a5d
Satyarth Mishra Sharma <mail@satyarth.me>
2015-10-08 17:58:20 +0300

    Make info.sh bar-agnostic

--- scripts/popups/info.sh
+++ scripts/popups/info.sh
@@ -44,8 +44,5 @@ currenttime() {
 }
 
 #!/bin/bash
-dzen2bg="#3f3f3f"
-dzen2fg="#ffffff"
-font='Tewi'
-panel=" $(currenttime) // $(battery) // $(temperature)"
-(echo $panel; sleep 2) | dzen2 -fn $font -fg $dzen2fg -bg $dzen2bg -x 32 -y 32 -w 216 -ta & sleep 2
+panel="$(currenttime) // $(battery) // $(temperature)"
+echo $panel
