12621dc012bc3abee27b65413c7c785f639818c9
Satyarth Mishra Sharma <mail@satyarth.me>
2015-10-08 18:36:35 +0300

    Fix string length calculation, ignores formatting characters

--- scripts/popup.sh
+++ scripts/popup.sh
@@ -5,9 +5,12 @@ source ~/dotfiles/colors/colors.sh
 font='-*-fixed-medium-*-*-*-12-*-*-*-*-*-*-*'
 
 read input_string
-strlen=${#input_string}
+
+stripped_string=`echo $input_string | sed ':again;$!N;$!b again; s/%{[^}]*}//g'`
+
+strlen=${#stripped_string}
 width=`echo $strlen*6+18 | bc`
 
 output_string="%{c}$input_string"
 
-(echo $output_string; sleep 2)| (lemonbar -g ${width}x20+32+32 -f $font -B$color2 -F#ff000000)
+(echo $output_string; sleep 2)| (lemonbar -g ${width}x20+32+32 -f $font -B#ff333333 -F$color2)
