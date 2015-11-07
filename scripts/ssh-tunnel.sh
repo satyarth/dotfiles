5a034476a875d6a58e2a5650467075953dd8e01e
Satyarth Mishra Sharma <mail@satyarth.me>
2015-11-07 14:03:13 +0300

    Add ssh-tunnel script

---
+++ scripts/ssh-tunnel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Source: https://juliansimioni.com/blog/howto-access-a-linux-machine-behind-a-home-router-with-ssh-tunnels/
+
+public_host=s@satyarth.me
+
+createTunnel() {
+    /usr/bin/ssh -f -N -R 10022:localhost:22 -L19922:$public_host:22 $public_host
+    if [[ $? -eq 0 ]]; then
+        echo Tunnel to HostA created successfully
+    else
+        echo An error occurred creating a tunnel to HostA. Return code: $?
+    fi
+}
+/usr/bin/ssh -p 19922 localhost ls > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo Creating new tunnel connection to HostA
+    createTunnel
+fi
